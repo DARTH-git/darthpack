6fbbfccf1a0b2042475ae15cefc55927fe52ac5c
zoekao <kaoxx085@umn.edu>
2019-07-16 14:00:02 -0500

    adding tests and check functions in simulation model

---
+++ tests/testthat/test_02_simulation_model_functions.R
@@ -0,0 +1,103 @@
+context("testing 02_simulation_model_functions.R")
+
+library(testthat)
+library(dplyr)    # For data manipulation
+library(darthpack)
+
+# load inputs
+l_params_all <- load_all_params()
+
+#### Unit tests start ####
+test_that("invalid number of cycles", {
+  l_params_all$n_t <- 90
+  
+  expect_error(decision_model(l_params_all, verbose = F), 
+               "Not all the age in the age range have a corresponding mortality rate")
+})
+
+test_that("invalid initial states", {
+  l_params_all$v_s_init <- c(H = -1, S1 = 0, S2 = 0, D = 0)
+  
+  expect_error(decision_model(l_params_all, verbose = F), 
+               "vector of initial states \\(v_s_init\\) is not valid")
+})
+
+test_that("reproducing error message invalid transition probabiliies", {
+  # generate testing data
+  for (i in 1:length(l_params_all)) {
+    assign(names(l_params_all)[[i]], l_params_all[[i]])
+  }
+  
+  # Mortality for healthy individuals
+  p_HDage  <- 1 - exp(-v_r_mort_by_age[(n_age_init + 1) + 0:(n_t - 1)])        
+  # Mortality for sick individuals
+  p_S1Dage <- 1 - exp(-v_r_mort_by_age[(n_age_init + 1) + 0:(n_t - 1)] * hr_S1)
+  # Mortality for sicker individuals
+  p_S2Dage <- 1 - exp(-v_r_mort_by_age[(n_age_init + 1) + 0:(n_t - 1)] * hr_S2)
+  
+  a_P <- array(0, dim = c(n_states, n_states, n_t),
+               dimnames = list(v_n, v_n, 0:(n_t-1)))
+  # Fill in array
+  # From H
+  a_P["H", "H", ]  <- (1-p_HDage) * (1 - p_HS1)
+  a_P["H", "S1", ] <- (1-p_HDage) * p_HS1
+  a_P["H", "D", ]  <- p_HDage
+  # From S1
+  a_P["S1", "H", ]  <- (1-p_S1Dage) * p_S1H
+  a_P["S1", "S1", ] <- (1-p_S1Dage) * (1 - (p_S1S2 + p_S1H))
+  a_P["S1", "S2", ] <- (1-p_S1Dage) * p_S1S2
+  a_P["S1", "D", ]  <- p_S1Dage
+  # From S2
+  a_P["S2", "S2", ] <- 1 - p_S2Dage
+  a_P["S2", "D", ]  <- p_S2Dage
+  # From D
+  a_P["D", "D", ] <- 1
+  
+  # check if the correct input might produce unintended message
+  expect_silent(check_transition_probability(a_P, err_stop = F, verbose = F))
+  expect_silent(check_transition_probability(a_P, err_stop = F, verbose = T))
+  expect_silent(check_transition_probability(a_P, err_stop = T, verbose = F))
+  expect_silent(check_transition_probability(a_P, err_stop = T, verbose = T))
+  
+  # check error messages of "check_transition_probability"
+  a_P2 <- a_P
+  a_P2["S2", "S2", ] <- -0.03
+  expect_warning(check_transition_probability(a_P2, err_stop = F, verbose = T))
+  expect_error(check_transition_probability(a_P2, err_stop = T, verbose = F))
+  expect_error(check_transition_probability(a_P2, err_stop = T, verbose = T))
+  
+  # check error messages of "check_sum_of_transition_array"
+  a_P2 <- a_P
+  a_P2["S2", "S2", ] <- -0.03
+  expect_warning(check_sum_of_transition_array(a_P2, n_states, n_t, err_stop = F, verbose = T))  
+  expect_error(check_sum_of_transition_array(a_P2, n_states, n_t, err_stop = T, verbose = T)) 
+  
+  # check the decision_model function
+  l_params_all2 <- l_params_all
+  l_params_all2$p_S1S2 <- -0.105
+  expect_silent(decision_model(l_params_all2, err_stop = F, verbose = F))
+  expect_error(decision_model(l_params_all2, err_stop = T, verbose = F))
+})
+
+test_that("correct outputs", {
+  output <- decision_model(l_params_all, err_stop = F, verbose = F)
+  
+  # checking overall outputs
+  expect_equal(length(output), 2)
+  expect_true(all(unlist(lapply(output, is.array))))
+  expect_identical(names(output), c("a_P", "m_M"))
+  
+  # checking output 1
+  expect_equal(dim(output[[1]]), 
+               c(l_params_all$n_states, l_params_all$n_states, l_params_all$n_t))
+  expect_true(all(output[[1]] >= 0) | all(output[[1]] <= 1))
+  expect_true(all(round(apply(output[[1]], c(1, 3), function(x) sum(x)) * 100) / 100 == 1))
+  
+  # checking output 2
+  expect_equal(dim(output[[2]]), 
+               c(l_params_all$n_t + 1, l_params_all$n_states))
+  expect_true(all(round(rowSums(output[[2]]) * 100) / 100 == 1))
+  expect_true(all(output[[2]] >= 0))
+  
+  output <- decision_model(l_params_all, err_stop = T, verbose = F)
+})
---
+++ tests/testthat/test_05_deterministic_analysis.R
@@ -0,0 +1,124 @@
+context("testing 05a_deterministic_analysis.R")
+
+library(testthat)
+
+library(reshape2)
+library(dampack)
+library(dplyr)
+library(darthpack)
+
+#### Generating relevant data and load functions ####
+l_params_all <- load_all_params()
+v_names_str <- l_params_all$v_names_str
+n_str <- length(v_names_str)
+l_params_basecase <- update_param_list(l_params_all, v_calib_post_map) 
+df_out_ce <- calculate_ce_out(l_params_all = l_params_basecase, 
+                              n_wtp = 150000)
+
+test_that("check icer table", {
+  # generate testing data
+  df_cea_det <- calculate_icers(cost       = df_out_ce$Cost, 
+                                effect     = df_out_ce$Effect, 
+                                strategies = v_names_str)
+  
+  # check data formate
+  expect_equal(nrow(df_cea_det), 2)
+  expect_equal(df_cea_det$Strategy, v_names_str)
+  expect_true(all(is.na(df_cea_det[1, c("Inc_Cost", "Inc_Effect", "ICER")])))
+})
+
+
+test_that("check one-way sensitivity output", {
+  # generate testing data
+  parms <- c("c_Trt", "p_HS1", "u_S1", "u_Trt")
+  ranges <- list("c_Trt" = c(6000, 13000),
+                 "p_HS1" = c(0.01, 0.50),
+                 "u_S1"  = c(0.75, 0.95),
+                 "u_Trt" = c(0.75, 0.95))
+  nsamps <- 100
+  n_wtp <- 150000
+  
+  owsa_nmb <- owsa_det(parms = parms, # parameter names
+                       ranges = ranges,
+                       nsamps = nsamps, # number of values  
+                       FUN = calculate_ce_out, # Function to compute outputs 
+                       params_basecase = l_params_basecase, # List with base-case parameters
+                       outcome = "NMB",      # Output to do the OWSA on
+                       strategies = v_names_str, # Names of strategies
+                       n_wtp = n_wtp       # Extra argument to pass to FUN
+  )
+  
+  # check samples 
+  expect_equal(nrow(owsa_nmb), length(parms) * length(v_names_str) * nsamps)
+  
+  # check whether the output has all the parameters specified
+  expect_true(all(unique(as.character(owsa_nmb$parameter)) %in% parms))
+  expect_equal(length(unique(as.character(owsa_nmb$parameter))), 4)
+  
+  # check whether the ranges of the output parameters are the same as the range specified
+  param_range <- owsa_nmb %>% 
+    select(parameter, param_val) %>% 
+    group_by(parameter) %>% 
+    summarize(range_min = min(param_val, na.rm = T), 
+              range_max = max(param_val, na.rm = T)) %>%
+    ungroup() %>% 
+    mutate(parameter = as.character(parameter))
+  
+  range_df <- do.call(rbind, ranges)
+  colnames(range_df) <- c("min", "max")
+  range_df <- as.data.frame(range_df)
+  range_df <- range_df[match(rownames(range_df), param_range$parameter), ]
+  
+  expect_true(all(range_df$max == param_range$range_max))
+  expect_true(all(range_df$min == param_range$range_min))
+  
+  # check owsa_nmb strategies
+  stgy_set <- unique(as.character(owsa_nmb$strategy))
+  expect_true(all(stgy_set %in% v_names_str))
+  
+  # check outcome_val
+  expect_true(all(is.numeric(owsa_nmb$outcome_val)))
+  expect_false(any(is.na(owsa_nmb$outcome_val)))
+})
+
+
+test_that("check two-way sensitivity output", {
+  # generate testing data
+  parm1 <- "u_S1"
+  parm2 <- "u_Trt"
+  ranges <- list("u_S1"  = c(0.70, 0.80),
+                 "u_Trt" = c(0.90, 1.00))
+  nsamps <- 40
+  twsa_nmb <- twsa_det(parm1 = parm1,  # parameter 1 name
+                       parm2 = parm2, # parameter 2 name
+                       ranges = ranges,
+                       nsamps = nsamps, # number of values  
+                       FUN = calculate_ce_out, # Function to compute outputs 
+                       params_basecase = l_params_basecase, # Vector with base-case parameters
+                       outcome = "NMB",      # Output to do the OWSA on
+                       strategies = v_names_str, # Names of strategies
+                       n_wtp = 150000        # Extra argument to pass to FUN
+  )
+    
+  # check samples
+  expect_equal(nrow(twsa_nmb), nsamps * nsamps * length(v_names_str))
+  
+  # check ranges
+  param_range <- data.frame(parms = c(parm1, parm2), 
+                            min = c(min(twsa_nmb$u_S1), min(twsa_nmb$u_Trt)), 
+                            max = c(max(twsa_nmb$u_S1), max(twsa_nmb$u_Trt)))
+  range_df <- do.call(rbind, ranges)
+  expect_equal(sum(range_df[, 1] - param_range[, "min"]), 0)
+  expect_equal(sum(range_df[, 2] - param_range[, "max"]), 0)
+  
+  # check strategies
+  expect_true(all(unique(as.character(twsa_nmb$strategy)) %in% v_names_str))
+  
+  # check outcome_val
+  expect_true(all(is.numeric(twsa_nmb$outcome_val)))
+  expect_false(any(is.na(twsa_nmb$outcome_val)))
+})
+
+
+
+
