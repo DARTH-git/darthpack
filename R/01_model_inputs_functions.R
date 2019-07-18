42960387c93032b3e59585f372f713d56a36ea40
Fernando Alarid-Escudero <alari006@umn.edu>
2019-07-18 13:38:47 -0500

    Removed 00_general_functions
    Moved teh functions from this script into inputs and det_analysis scripts.

--- R/01_model_inputs_functions.R
+++ R/01_model_inputs_functions.R
@@ -70,3 +70,22 @@ load_all_params <- function(file.init = NULL,
   l_params_all <- c(l_params_all, 
                     df_params_init) # Add initial set of parameters
 }
+
+#' Update parameters
+#'
+#' \code{update_param_list} is used to update list of all parameters with new 
+#' values for specific parameters.
+#'
+#' @param l_params_all List with all parameters of decision model
+#' @param params_updated Parameters for which values need to be updated
+#' @return 
+#' A list with all parameters updated.
+#' @export
+update_param_list <- function(l_params_all, params_updated){
+  
+  if (typeof(params_updated)!="list"){
+    params_updated <- split(unname(params_updated),names(params_updated)) #converte the named vector to a list
+  }
+  l_params_all <- modifyList(l_params_all, params_updated) #update the values
+  return(l_params_all)
+}
